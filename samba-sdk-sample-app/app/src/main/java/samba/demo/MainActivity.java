1680dc28961f319d9b468d615bc813e7e7fd54c3
Cristian Calugar <cristianc@imprezzio.com>
2018-03-02 16:28:47 +0200

    Refactoring on MainActivity

--- samba-sdk-sample-app/app/src/main/java/samba/demo/MainActivity.java
+++ samba-sdk-sample-app/app/src/main/java/samba/demo/MainActivity.java
@@ -14,11 +14,10 @@ import android.widget.TextView;
 
 import com.samba.Samba;
 import com.samba.SambaConfig;
-import com.samba.SambaSetup;
 import com.samba.SambaListener;
+import com.samba.SambaSetup;
 
 import java.util.UUID;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,64 +52,59 @@ public class MainActivity extends AppCompatActivity {
         publisherIdTextView.setText(getString(R.string.text_secret_key, SECRET_KEY));
 
         Button initBtn = findViewById(R.id.init_btn);
-        initBtn.setOnClickListener(getOnClickListener());
+        initBtn.setOnClickListener(onClickListener);
 
         mBtnLoad = findViewById(R.id.load_btn);
-        mBtnLoad.setOnClickListener(getOnClickListener());
+        mBtnLoad.setOnClickListener(onClickListener);
 
         mBtnShow = findViewById(R.id.show_btn);
-        mBtnShow.setOnClickListener(getOnClickListener());
+        mBtnShow.setOnClickListener(onClickListener);
 
         mProgressBar = findViewById(R.id.progress_bar);
 
         mOptimizeDownloadOnMobileNetworkSwitch = findViewById(R.id.optimize_download_switch);
         mOptimizeDownloadOnMobileNetworkSwitch.setChecked(true);
-        mOptimizeDownloadOnMobileNetworkSwitch.setOnCheckedChangeListener(getOnCheckedChangedListener());
+        mOptimizeDownloadOnMobileNetworkSwitch.setOnCheckedChangeListener(onCheckedChangedListener);
 
         mDisableSoundAtVideoStartSwitch = findViewById(R.id.disable_sound_switch);
-        mDisableSoundAtVideoStartSwitch.setOnCheckedChangeListener(getOnCheckedChangedListener());
+        mDisableSoundAtVideoStartSwitch.setOnCheckedChangeListener(onCheckedChangedListener);
     }
 
-    private View.OnClickListener getOnClickListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int id = view.getId();
-                switch (id) {
-                    case R.id.init_btn: {
-                        init();
-                        break;
-                    }
-                    case R.id.load_btn: {
-                        load();
-                        break;
-                    }
-                    case R.id.show_btn: {
-                        show();
-                        break;
-                    }
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+            switch (id) {
+                case R.id.init_btn: {
+                    init();
+                    break;
+                }
+                case R.id.load_btn: {
+                    load();
+                    break;
+                }
+                case R.id.show_btn: {
+                    show();
+                    break;
                 }
             }
-        };
-    }
+        }
+    };
 
-    private CompoundButton.OnCheckedChangeListener getOnCheckedChangedListener() {
-        return new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                switch (compoundButton.getId()) {
-                    case R.id.optimize_download_switch:
-                        SambaConfig.optimizeDownloadOnMobileNetwork(checked);
-                        break;
-                    case R.id.disable_sound_switch:
-                        //when switch is checked set video sound disabled when starting video
-                        SambaConfig.setSoundDisabled(checked);
-                        break;
-                }
-
+    private CompoundButton.OnCheckedChangeListener onCheckedChangedListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            switch (compoundButton.getId()) {
+                case R.id.optimize_download_switch:
+                    SambaConfig.optimizeDownloadOnMobileNetwork(checked);
+                    break;
+                case R.id.disable_sound_switch:
+                    //when switch is checked set video sound disabled when starting video
+                    SambaConfig.setSoundDisabled(checked);
+                    break;
             }
-        };
-    }
+        }
+    };
 
     private void init() {
         //Optimize mobile download on mobile when starting activity
@@ -126,6 +120,48 @@ public class MainActivity extends AppCompatActivity {
         mBtnLoad.setEnabled(true);
     }
 
+    private void load() {
+        //Get samba instance
+        mSamba = Samba.getInstance(this);
+
+        //Set a samba listener in order to register for sdk events
+        mSamba.setSambaListener(sambaListener);
+
+        //Start loading samba ads
+        mSamba.loadAd();
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private SambaListener sambaListener = new SambaListener() {
+        @Override
+        public void onAdLoadSuccess() {
+            //Run the ui related code on UI thread as callback may not return on main thread
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mBtnShow.setEnabled(true);
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            });
+        }
+
+        @Override
+        public void onAdLoadFail(@NonNull String s) {
+
+        }
+
+        @Override
+        public void onAdDidReachEnd(boolean wasSuccessfulView) {
+            super.onAdDidReachEnd(wasSuccessfulView);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mBtnShow.setEnabled(false);
+                }
+            });
+        }
+    };
+
     private void show() {
         //Once samba ad is loaded, show the ad
         if (mSamba.isAdLoaded()) {
@@ -133,43 +169,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void load() {
-        //Get samba instance
-        mSamba = Samba.getInstance(this);
-
-        //Set a samba listener in order to register for sdk events
-        mSamba.setSambaListener(new SambaListener() {
-            @Override
-            public void onAdLoadSuccess() {
-                //Run the ui related code on UI thread as callback may not return on main thread
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mBtnShow.setEnabled(true);
-                        mProgressBar.setVisibility(View.GONE);
-                    }
-                });
-            }
-
-            @Override
-            public void onAdLoadFail(@NonNull String s) {
-
-            }
-
-            @Override
-            public void onAdDidReachEnd(boolean wasSuccessfulView) {
-                super.onAdDidReachEnd(wasSuccessfulView);
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mBtnShow.setEnabled(false);
-                    }
-                });
-            }
-        });
-
-        //Start loading samba ads
-        mSamba.loadAd();
-        mProgressBar.setVisibility(View.VISIBLE);
-    }
 }
